75ebaf447f1e09b17290b9e27e3da202433a4fee
Teppo <teppo@protonmail.com>
2022-04-15 13:25:33 +0300

    Updated the use of colours in svr_plot_numvar() and svr_plot_numvar_facet()

--- R/plot_numvar.R
+++ R/plot_numvar.R
@@ -2,7 +2,11 @@
 
 svr_plot_numvar <- function(
     df.singlevar,
-    box_color = 'darkgrey'
+    box_color = 'darkgrey',
+    violin_color = '#6db6ff',
+    mean_color = '#db6d00',
+    point_alpha = 0.1,
+    box_width = 0.5
 ) {
 
     df.singlevar %>%
@@ -11,22 +15,22 @@ svr_plot_numvar <- function(
 
         ggplot( mapping = aes( x = value, y = name ) ) +
 
-        geom_violin( fill = col_qua15['c5'] ) +
+        geom_violin( fill = violin_color ) +
 
-        geom_jitter( alpha = 0.1 ) +
+        geom_jitter( alpha = point_alpha ) +
 
         geom_boxplot(
             color = box_color,
             # Make the box transparent
             alpha = 0,
-            # Use width to fit the box inside the violin
-            width = 0.5
+            # Fit the box inside the violin
+            width = box_width
         ) +
 
         stat_summary(
             fun = 'mean',
             geom = 'point',
-            color = 'red'
+            color = mean_color
         ) +
 
         svr_theme_vertical() +
@@ -42,14 +46,24 @@ svr_plot_numvar <- function(
 svr_plot_numvar_facet <- function(
     df,
     varnames,
-    box_color = 'darkgrey'
+    box_color = 'darkgrey',
+    violin_color = '#6db6ff',
+    mean_color = '#db6d00',
+    point_alpha = 0.1,
+    box_width = 0.5
 ) {
 
     df %>%
 
         select( all_of( varnames ) ) %>%
 
-        svr_plot_numvar() +
+        svr_plot_numvar(
+            box_color = box_color,
+            violin_color = violin_color,
+            mean_color = mean_color,
+            point_alpha = point_alpha,
+            box_width = box_width
+        ) +
 
         facet_wrap(
             ~name,
